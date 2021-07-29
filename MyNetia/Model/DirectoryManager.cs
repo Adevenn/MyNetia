53ff6fd8545c26ed1732b439cfcc7b4956347984
Adrien Evennou <adrien.ev@hotmail.com>
2021-07-29 22:41:55 +0200

    Update old classes

--- MyNetia/Model/DirectoryManager.cs
+++ MyNetia/Model/DirectoryManager.cs
@@ -7,27 +7,6 @@ namespace MyNetia.Model
     public static class DirectoryManager
     {
         public static string DESKTOP = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        public static void createDirectory(string name)
-        {
-            string path = Path.GetFullPath(@".\AppResources\Images\" + name);
-            try { Directory.CreateDirectory(path); }
-            catch (ArgumentException e) { throw new ArgumentException("Invalid path (some characters are not allowed:\n\n" + e.Message); }
-        }
-
-        public static void deleteDirectory(string name)
-        {
-            string path = Path.GetFullPath(@".\AppResources\Images\" + name);
-            try { Directory.Delete(path, true); }
-            catch (DirectoryNotFoundException e) { throw new DirectoryNotFoundException("Path is incorrect:\n\n" + e.Message); }
-        }
-
-        public static void renameDirectory(string oldName, string name)
-        {
-            string path = Path.GetFullPath(@".\AppResources\Images\");
-            try { Directory.Move(path + oldName, path + name); }
-            catch (ArgumentException e) { throw new ArgumentException("Invalid path (some characters are not allowed:\n\n" + e.Message); }
-            catch (DirectoryNotFoundException e) { throw new DirectoryNotFoundException("Path is incorrect:\n\n" + e.Message); }
-        }
 
         /// <summary>
         /// Return true if the name is valid, return false if the name is not valid
--- MyNetia/Model/FileManager.cs
+++ MyNetia/Model/FileManager.cs
@@ -5,37 +5,32 @@ namespace MyNetia.Model
     public static class FileManager
     {
         private static readonly string defaultImgPath = Path.GetFullPath(@".\AppResources\Images\DefaultImage.png");
-        private static readonly string imagesPath = Path.GetFullPath(@".\AppResources\Images\");
 
-        private static void createTxtFile(string path, string content)
-        {
-            try { File.WriteAllText(path, content); }
-            catch (IOException e) { throw new IOException("Text file creation failed:\n\n" + e.Message); }
-        }
-
+        /// <summary>
+        /// Read all bytes from a file
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public static byte[] readByteFile(string path)
         {
             try { return File.ReadAllBytes(path); }
-            catch (IOException e) { throw new IOException("Read byte file failed:\n\n" + e.Message); }
+            catch (IOException e) { throw new IOException("Read byte[] file failed:\n\n" + e.Message); }
         }
 
-        public static string readTxtFile(string path)
-        {
-            try { return File.ReadAllText(path); }
-            catch (IOException e) { throw new IOException("Read text file failed:\n\n" + e.Message); }
-        }
-
-        private static void copyFile(string sourceFile, string destFile)
-        {
-            try { File.Copy(sourceFile, destFile, true); }
-            catch (IOException e) { throw new IOException("File copy failed\n\n" + e.Message); }
-        }
-
+        /// <summary>
+        /// Return the file name from a path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public static string getFileName(string path)
         {
             return Path.GetFileName(path);
         }
 
+        /// <summary>
+        /// Return the default image
+        /// </summary>
+        /// <returns></returns>
         public static byte[] defaultImage()
         {
             return readByteFile(defaultImgPath);
